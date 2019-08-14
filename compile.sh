77478f6665f8563ab289da143fe916cef98de2f1
Venkatesh Bolla <venktesh.bolla@gmail.com>
2019-08-14 14:53:10 +0530

    Update
    Signed-off-by: Venkatesh Bolla <venktesh.bolla@gmail.com>

--- compile.sh
+++ compile.sh
@@ -1,3 +1,6 @@
-#sets custom cmopiled clang version
+#sets custom cmopiled clang version 5.0.2
+#kernel version needed for xdp 5.0.7
+#iproute2 4.11.0
+#Google "julia evans xdp" for reference
 venv
 make
